7669374c0a94685573d5dd2d7ccbad5372feb466
232014092Asma <asma.ahamed.cse@ulab.edu.bd>
2025-08-21 20:10:50 +0600

    first commit

---
+++ Task1.js
@@ -0,0 +1,21 @@
+// Task 1
+let name = "Rahim";
+let age = 20;
+let isStudent = true;
+
+// Task 2
+let futureAge = age + 5;
+console.log("Age after 5 years:", futureAge);
+
+// Task 3
+let a = 10;
+let b = 5;
+console.log("Addition:", a + b);
+console.log("Subtraction:", a - b);
+console.log("Multiplication:", a * b);
+console.log("Division:", a / b);
+
+// Task 4
+console.log("Type of name:", typeof name);
+console.log("Type of age:", typeof age);
+console.log("Type of isStudent:", typeof isStudent);
